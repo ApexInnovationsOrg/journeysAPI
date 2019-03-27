a2a348ef466cbac1930ae0a5f48edc7f03b221b6
eddimull <eddimull@gmail.com>
2019-03-27 14:34:36 -0500

    backend CRUD for creating content for the DB

--- controllers/Content.php
+++ controllers/Content.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Capsule\Manager as DB;
 use Dotenv\Dotenv as Dotenv;
 
 
-class Question
+class Content
 {
     private $_params;
     private $data;
@@ -23,10 +23,28 @@ class Question
         
     }
 
-    public function createNewMediaAction()
+    public function createNewContentAction()
     {
+        error_log('creating new content');
+        
         $write = $this->pdo->prepare("INSERT INTO journey_content (QuestionID,Content) VALUES (:questionID,:content)");
-        $write->execute([':questionID'=>$this->data['questionID'],':content'=>htmlspecialchars_decode(htmlentities($data['content'], ENT_NOQUOTES, 'UTF-8'), ENT_NOQUOTES));
+        $write->execute([':questionID'=>$this->data['questionID'],':content'=>$this->data['content']]);
+
+        return $this->pdo->lastInsertId();
+    }
+    
+    public function deleteContentAction()
+    {
+        $write = $this->pdo->prepare("UPDATE journey_content SET QuestionID = QuestionID * -1 WHERE ID = :questionID");
+        $write->execute([':questionID'=>$this->data['questionID']]);
+    }
+    
+    public function updateContentAction()
+    {
+        $write = $this->pdo->prepare("UPDATE journey_content SET Content = :content WHERE ID = :contentID");
+        $write->execute([':contentID'=>$this->data['contentID'],':content'=>$this->data['content']]);
+        
+        return $this->pdo->lastInsertId();
     }
 
     
